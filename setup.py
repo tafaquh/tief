e0a4116c47218cc6f9c7be0193eb6760e559b6a9
tafaquh <tafaquh.fiddinal@gmail.com>
2020-05-19 16:06:01 +0700

    Release Association Rule

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
+# M Tafaquh Fiddin Al Islami 2020
+# TieF AI Tafaquh Fiddin Al Islami Library
+# Author: M Tafaquh Fiddin Al Islami
+#
+# License: MIT
+
+
 from distutils.core import setup
+
 setup(
   name = 'tief',         # How you named your package folder (MyLib)
   packages = ['tief'],   # Chose the same as "name"
@@ -8,20 +16,17 @@ setup(
   author = 'M Tafaquh Fiddin Al Islami',                   # Type in your name
   author_email = 'tafaquh.fiddinal@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
+  download_url = 'https://github.com/tafaquh/tief/archive/0.1.tar.gz',    # I explain this later on
+  keywords = ['AI', 'Machine Learning', 'TIEF', 'Library'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
-      ],
-  classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
+    'numpy',
+    'pandas',
+    'itertools',
   ],
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+  ],
+  python_requires='>=3.6',
 )
