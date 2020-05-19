02bd54203a5469791410048e9419fa3103c53300
tafaquh <tafaquh.fiddinal@gmail.com>
2020-05-19 16:16:51 +0700

    Update __init__.py

---
+++ tief/__init__.py
@@ -0,0 +1,10 @@
+# M Tafaquh Fiddin Al Islami 2020
+# TieF AI Tafaquh Fiddin Al Islami Library
+# Author: M Tafaquh Fiddin Al Islami
+#
+# License: MIT
+
+from .association.apriori import apriori
+from .association.association_rule import association_rules
+
+__all__ = ["apriori", "association_rules"]
